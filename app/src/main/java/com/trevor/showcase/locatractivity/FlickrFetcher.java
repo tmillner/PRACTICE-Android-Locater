95cb1a36cbe915ffdda41fabb88cb3e0602139f4
Trevor Millner <trevormillner@gmail.com>
2018-03-05 02:58:48 -0800

    init

---
+++ app/src/main/java/com/trevor/showcase/locatractivity/FlickrFetcher.java
@@ -0,0 +1,143 @@
+package com.trevor.showcase.locatractivity;
+
+import android.location.Location;
+import android.net.Uri;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by trevormillner on 1/24/18.
+ */
+public class FlickrFetcher {
+
+    private static final String TAG = FlickrFetcher.class.getName();
+
+    private static final String API_KEY = "7da8cff0093d73af52a9f7c652562a7e";
+
+    private static final String RECENT_PHOTOS_METHOD = "flickr.photos.getRecent";
+    private static final String SEARCH_PHOTOS_METHOD = "flickr.photos.search";
+
+    private static final Uri ENDPOINT = Uri.parse("https://api.flickr.com/services/rest/")
+            .buildUpon()
+            .appendQueryParameter("api_key", API_KEY)
+            .appendQueryParameter("format", "json")
+            .appendQueryParameter("nojsoncallback", "1")
+            .appendQueryParameter("extras", "url_s")
+            .build();
+
+    public byte[] getURLBytes(String urlSpec) throws IOException {
+        URL url = new URL(urlSpec);
+        // This allows us a clean interface to operate on HTTP resources
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            // At this point connects to the endpoint
+            InputStream inputStream = connection.getInputStream();
+
+            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                throw new IOException(connection.getResponseMessage() +
+                        ": with " + urlSpec);
+            }
+
+            // Read in the bytes from in, into the out ByteArrayOutputStream
+            int bytesRead = 0;
+            byte[] buffer = new byte[1024];
+            while ((bytesRead = inputStream.read(buffer)) > 0) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+            outputStream.close();
+            return outputStream.toByteArray();
+        } finally {
+            connection.disconnect();
+        }
+    }
+
+    public String getURLString(String urlSpec)  throws IOException  {
+        return new String(getURLBytes(urlSpec));
+    }
+
+    public List<GalleryItem> downloadGalleryItems(String url) {
+
+        List<GalleryItem> items = new ArrayList<>();
+
+        try {
+           String jsonResponse = getURLString(url);
+           Log.i(TAG, "Fetch flickr recent items result: " + jsonResponse);
+           JSONObject jsonObject = new JSONObject(jsonResponse);
+           parseRecentItems(items, jsonObject);
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to Fetch flickr recent items: " + e);
+        } catch (JSONException e) {
+            Log.e(TAG, "Failed to parse json for Fetch flickr recent items: " + e);
+        }
+
+        return items;
+    }
+
+    public List<GalleryItem> fetchRecentItems() {
+        String url =  buildUrl(RECENT_PHOTOS_METHOD, null);
+        return downloadGalleryItems(url);
+    }
+
+    public List<GalleryItem> searchPhotos(String query) {
+        String url = buildUrl(SEARCH_PHOTOS_METHOD, query);
+        return downloadGalleryItems(url);
+    }
+
+    public List<GalleryItem> searchPhotos(Location location) {
+        String url = buildUrl(location);
+        return downloadGalleryItems(url);
+    }
+
+    private String buildUrl(Location location) {
+        return ENDPOINT.buildUpon()
+                .appendQueryParameter("method", SEARCH_PHOTOS_METHOD)
+                .appendQueryParameter("lat", "" + location.getLatitude())
+                .appendQueryParameter("lon", "" + location.getLongitude())
+                .build().toString();
+    }
+
+    private String buildUrl(String method, String query) {
+       Uri.Builder uriBuilder = ENDPOINT.buildUpon()
+               .appendQueryParameter("method", method);
+
+       if (method == SEARCH_PHOTOS_METHOD) {
+           uriBuilder.appendQueryParameter("text", query);
+       }
+
+       return uriBuilder.toString();
+    }
+
+    public void parseRecentItems(List<GalleryItem> items, JSONObject jsonObject)
+            throws JSONException{
+        JSONObject photosObject = jsonObject.getJSONObject("photos");
+        JSONArray photosArray = photosObject.getJSONArray("photo");
+
+        for (int i = 0; i < photosArray.length(); i++) {
+            JSONObject photo = photosArray.getJSONObject(i);
+
+            GalleryItem item = new GalleryItem();
+            item.setId(photo.getString("id"));
+            item.setCaption(photo.getString("title"));
+
+            if(!photo.has("url_s")) {
+                continue;
+            }
+
+            item.setUrl(photo.getString("url_s"));
+            items.add(item);
+        }
+    }
+}
---
+++ app/src/main/java/com/trevor/showcase/locatractivity/LocatrFragment.java
@@ -0,0 +1,157 @@
+package com.trevor.showcase.locatractivity;
+
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class LocatrFragment extends Fragment {
+
+    private static final String TAG = LocatrFragment.class.getName();
+
+    private ImageView mImageView;
+    private GoogleApiClient mGoogleApiClient;
+
+    public static LocatrFragment newInstance() {
+        return new LocatrFragment();
+    }
+
+    public LocatrFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_locatr, container, false);
+        ImageView imageView = v.findViewById(R.id.image);
+
+        return v;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        getActivity().invalidateOptionsMenu();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                    @Override
+                    public void onConnected(Bundle bundle) {
+                        getActivity().invalidateOptionsMenu();
+                    }
+
+                    @Override
+                    public void onConnectionSuspended(int i) {
+
+                    }
+                })
+                .build();
+
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_locatr, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_locate);
+        searchItem.setEnabled(mGoogleApiClient.isConnected());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.action_locate:
+                findImage();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void findImage() {
+        LocationRequest request = LocationRequest.create();
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        request.setNumUpdates(1);
+        request.setInterval(0);
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, request, new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        Log.i(TAG, "Got fix: " + location );
+                        new SearchTask().doInBackground(location);
+                    }
+                });
+    }
+
+    public class SearchTask extends AsyncTask<Location, Void, Void> {
+        private GalleryItem mGalleryItem;
+        private Bitmap mBitmap;
+
+        @Override
+        protected Void doInBackground(Location... locations) {
+            FlickrFetcher flickrFetcher = new FlickrFetcher();
+            List<GalleryItem> photos = flickrFetcher.searchPhotos(locations[0]);
+
+            if(photos.size() == 0) return null;
+
+            mGalleryItem = photos.get(0);
+
+            try {
+                byte[] bytes = flickrFetcher.getURLBytes(mGalleryItem.getUrl());
+                mBitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+            } catch (IOException e) {
+                Log.e(TAG, "Unable to download image from: " + mGalleryItem.getUrl() +
+                        " " + e);
+                e.printStackTrace();
+            }
+
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            mImageView.setImageBitmap(mBitmap);
+        }
+    }
+}
